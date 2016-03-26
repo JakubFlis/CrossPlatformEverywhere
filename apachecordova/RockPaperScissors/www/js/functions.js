70846a808347b13e4903d8422dbca13380e6ba29
Jakub Flis <jkb.flis@gmail.com>
2016-03-26 19:49:26 +0100

    Added Rock Paper Scissors game prototype

---
+++ apachecordova/RockPaperScissors/www/js/functions.js
@@ -0,0 +1,62 @@
+function handleUserChoice(userChoice) {
+    //0 - rock, 1 - paper, 2 - scissors
+    var computerChoice = getRandomInt(0, 2);
+    var computerElement = document.getElementById("computer-choice");
+    var userElement = document.getElementById("user-choice");
+   
+    switch (userChoice) {
+        case 0:
+            if (computerChoice == 0) {
+                handleUserChoice("rock");
+            } else if (computerChoice == 1) {
+                showResultsToUser(false);
+                computerElement.innerHTML = "Computer: PAPER";
+            } else {
+                showResultsToUser(true);
+                computerElement.innerHTML = "Computer: SCISSORS";
+            }
+            
+            userElement.innerHTML = "You: ROCK";
+        break;
+        case 1:
+            if (computerChoice == 1) {
+                handleUserChoice("paper");
+            } else if (computerChoice == 0) {
+                showResultsToUser(true);
+                computerElement.innerHTML = "Computer: ROCK";
+            } else {
+                showResultsToUser(false);
+                computerElement.innerHTML = "Computer: SCISSORS";
+            }
+            
+            userElement.innerHTML = "You: PAPER";
+        break;
+        case 2:
+            if (computerChoice == 2) {
+                handleUserChoice("scissors");
+            } else if (computerChoice == 1) {
+                showResultsToUser(true);
+                computerElement.innerHTML = "Computer: PAPER";
+            } else {
+                showResultsToUser(false);
+                computerElement.innerHTML = "Computer: ROCK";
+            }
+            
+            userElement.innerHTML = "You: SCISSORS";
+        break;
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function showResultsToUser(didUserWin) {
+    var result = document.getElementById("result");
+    
+    if (didUserWin) {
+        result.innerHTML = "YOU WON!";
+    } else {
+        result.innerHTML = "YOU LOST!";
+    }
+}
